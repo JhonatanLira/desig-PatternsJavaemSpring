d19bb936710e6cd8480920d61bebad691fb015b7
jhonatan <jhonatanlira@gmail.com>
2024-07-22 07:09:07 -0300

    Criando models

---
+++ src/main/java/one/digitalinnovation/gof/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,62 @@
+package one.digitalinnovation.gof.service.impl;
+
+import java.util.Optional;
+
+import one.digitalinnovation.gof.model.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import one.digitalinnovation.gof.model.Cliente;
+import one.digitalinnovation.gof.model.Endereco;
+import one.digitalinnovation.gof.model.EnderecoRepository;
+import one.digitalinnovation.gof.service.ClienteService;
+import one.digitalinnovation.gof.service.ViaCepService;
+
+/**
+ * Implementação da <b>Strategy</b> {@link ClienteService}, a qual pode ser
+ * injetada pelo Spring (via {@link Autowired}). Com isso, como essa classe é um
+ * {@link Service}, ela será tratada como um <b>Singleton</b>.
+ *
+ * @author falvojr
+ */
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+    // Singleton: Injetar os componentes do Spring com @Autowired.
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepService viaCepService;
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }s
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+
+    // Strategy: Implementar os métodos definidos na interface.
+    // Facade: Abstrair integrações com subsistemas, provendo uma interface simples.
+
+
+}
